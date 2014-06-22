4da0d0947816d33f26e00569b04cfbc10234f52a
Sergeij Belevskij <bsa7@inbox.ru>
2014-06-22 06:47:45 +0600

    add webp compression

--- ligtweight_me.rb
+++ ligtweight_me.rb
@@ -9,5 +9,10 @@ Dir.glob("**/*").each do |entryname|
   elsif entryname =~ /\.jpe?g$/i
     system "convert #{entryname} -quality 1 #{entryname}"
     puts "#{entryname.to_s.green} - converted"
+  elsif entryname =~ /\.webp$/i
+    system "dwebp -v #{entryname} -o tmpl.png"
+    system "convert tmpl.png -depth 2 tmpl.png"
+    system "cwebp -q -v 1 tmpl.png -o #{entryname}"
+    puts "#{entryname.to_s.cyan} - converted"
   end
 end
